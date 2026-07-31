04e18f0c9f86627609e74a11f9850b6cc88aada3
thisisouvik <souvikmandals10@gmail.com>
2026-07-31 11:49:26 +0530

    feat: update ESLint configuration to disable specific TypeScript rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,14 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "prefer-const": "off",
+    }
+  }
 ]);
 
 export default eslintConfig;
